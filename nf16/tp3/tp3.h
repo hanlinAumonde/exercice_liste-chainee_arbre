88a117fe52473a718f42e56f5647b48345703f82
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-04-18 20:16:25 +0200

    Update tp3.h

--- nf16/tp3/tp3.h
+++ nf16/tp3/tp3.h
@@ -15,9 +15,14 @@ typedef struct matrice_creuse {
     int Ncolonnes;
 };
 
-void remplirMat(matrice_creuse *m, int N, int M);
+void remplirMat(struct matrice_creuse *m, int N, int M);
 
-void afficherMat(matrice_creuse m);
+void afficherMat(struct matrice_creuse m);
 
-int getValue(matrice_creuse m, int i, int j);
+int getValue(struct matrice_creuse m, int i, int j);
 
+void putValue(struct matrice_creuse m, int i, int j,int val);
+
+void addMat(struct matrice_creuse m1,struct matrice_creuse m2);
+
+int nombreOctetsGagnes(struct matrice_creuse m);
