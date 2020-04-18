e53da473342007440434ba3268dd8be39d9dc3f7
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-04-18 14:36:33 +0200

    Update tp3.c

--- nf16/tp3/tp3.c
+++ nf16/tp3/tp3.c
@@ -7,18 +7,19 @@
 //Q3
 int getValue(struct matrice_creuse m,int i,int j){
     int res;
-    ligne_liste first=m.lignes[i];
-    int k=0;
-    if(m.lignes[i] == NULL){//si le premier est null,toutes les valeurs dans cette ligne sont 0,y compris celui que nous recherchons
+    ligne_liste it=m.lignes[i];
+    if(m.lignes[i] == NULL){//si le premier node est null,toutes les valeurs dans cette ligne sont 0,y compris celui que nous recherchons
         res=0;
     }
        else{
-           while(k<=j){
-             if(first->col == j){res=first->val;}
-             else {first=first->next;}
-             k++;
-           }
-       }
-    return res;
-}
+           while(it!=NULL && it->col<=j){        //1er cas: le résultat est 0,et j est avant le premier node du m.ligne[i]
+             if(it->col == j){res=it->val;}      //2eme cas:le résultat est "it->val",et itt->col=j
+             else {it=it->next;}                 //3eme cas:le résultat est 0,et j compris entre deux node du m.ligne[i] 
+           }                                           //4eme cas:le résultat est 0,et j est apres le dernier node du m.ligne[i]
+           if(it->col<>j || it=NULL) {res=0;}
+       }                                               //*on a le 2eme cas dans le boucle while,
+    return res;                                        //*pour le 1er cas,puisque la condition 'it->col<=j' est faux au début,le boucle ne s'exécutera pas,pcq it->col<>j,on a res=0
+}                                                      //*si le boucle peut s'exécuter,apres le boucle,'it->col<>j' correspond au 3eme cas,'it=NULL' correspond au 4eme cas
+
+
 
