6a55e306a3449911a2226475456656c0b44d3d42
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-04-20 20:32:50 +0200

    Update tp3.h

--- nf16/tp3/tp3.h
+++ nf16/tp3/tp3.h
@@ -1,11 +1,16 @@
-#ifndef TP3_H
-#define TP3_H
-#endif //TP3_H
+#ifndef TTP3_H
+#define TTP3_H
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 typedef struct element{
     int col;
     int val;
     struct element *next;
-} element;
+}element;
 
 typedef element *liste_ligne;
 
@@ -13,7 +18,7 @@ typedef struct matrice_creuse {
     liste_ligne *lignes;
     int Nlignes;
     int Ncolonnes;
-};
+}matrice_creuse;
 
 void remplirMat(struct matrice_creuse *m, int N, int M);
 
@@ -26,3 +31,6 @@ void putValue(struct matrice_creuse m, int i, int j,int val);
 void addMat(struct matrice_creuse m1,struct matrice_creuse m2);
 
 int nombreOctetsGagnes(struct matrice_creuse m);
+
+#endif
+
