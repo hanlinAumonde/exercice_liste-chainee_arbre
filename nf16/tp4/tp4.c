319121970dea72d947c6ff6d01a674c151704b3d
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-05-12 12:15:33 +0200

    Upload New File

---
+++ nf16/tp4/tp4.c
@@ -0,0 +1,55 @@
+#include "tp4.h"
+
+//Q1
+T_Arbre initABR() {
+    T_Arbre abr = malloc(sizeof(T_Noeud));
+    return abr;
+}
+
+//Q2
+T_Noeud *creerNoeud(int cle, int occ) {
+    T_Noeud *noeud = malloc(sizeof(T_Noeud));
+    noeud->cle = cle;
+    noeud->nb_o = occ;
+    noeud->gauche = NULL;
+    noeud->droite = NULL;
+    return noeud;
+}
+
+//Q3
+void ajoutElement(T_Arbre abr , int cle, int occ){
+    if(abr == NULL){//si il n'y a pas encore d'arbre
+        abr = initABR();
+        abr->Racine = creerNoeud(cle,occ);
+    }
+    T_Noeud *node = creerNoeud(cle,occ);
+    if(cle < abr->Racine->cle){  //partie gauche de l'arbre
+        T_Noeud *it1 = abr->Racine->gauche;
+        while(it1 != NULL){   //trouver la position pour ajout un nouveau noeud
+            if(cle < it1->cle){
+              if(it1->gauche == NULL){it1->gauche = node;}
+              else{it1 = it1->gauche;}
+            }
+            else{
+              if(it1->droite == NULL){it1->droite = node;}
+              else{it1 = it1->droite;}
+              }
+        }
+    }
+    else if(cle > abr->Racine->cle){ //partie droite,similaire au cas precedent
+        T_Noeud *it2 = abr->Racine->droite;
+        while(it2 != NULL){
+            if(cle < it2->cle){
+              if(it2->gauche == NULL){it2->gauche = node;}
+              else{it2 = it2->gauche;}
+            }
+            else{
+              if(it2->droite == NULL){it2->droite = node;}
+              else{it2 = it2->droite;}
+              }
+        }
+    }
+}
+
+//Q4
+void afficherArbre
