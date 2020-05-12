228024e95bca6f0a86008ed696f1bfc7c20494ac
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-05-12 22:15:04 +0200

    Update tp4.c

--- nf16/tp4/tp4.c
+++ nf16/tp4/tp4.c
@@ -2,7 +2,7 @@
 
 //Q1
 T_Arbre* initABR() {
-    T_Arbre *abr = malloc(sizeof(T_Noeud));
+    T_Arbre *abr = malloc(sizeof(T_Arbre));
     abr=NULL;
     return abr;
 }
@@ -34,6 +34,7 @@ void ajoutElement(T_Arbre *abr , int cle, int occ){
               }
         }
 
+
 }
 
 //Q4
@@ -62,4 +63,65 @@ T_Noeud* rechercherElement(T_Arbre *abr , int val){
 }
 
 //Q6
-void decrementerElement(T_Arbre *abr){}
+static T_Noeud *pere(T_Arbre *abr,T_Noeud *n){  //pere d'un noeud
+    T_Noeud *it=abr->Racine;
+    if(n == it) return NULL;
+    while(it!=NULL && n->cle!=it->gauche->cle && n->cle!=it->droite->cle){
+        if(it->cle > n->cle){it=it->gauche;}
+        else if(it->cle > n->cle){it=it->droite;}
+    }
+    return it;
+}
+
+static T_Noeud *min(T_Arbre *abr,T_Noeud *n){  //ArbreMin pour trouver le successeur
+    while(n->gauche!=NULL){n = n->gauche;}
+    return n;
+}
+
+static T_Noeud *successeur(T_Arbre *abr,T_Noeud *n){ //successeur
+    if(n->droite != NULL){
+        while(n->droite->gauche != NULL){return min(abr,n->droite);}
+    }
+    T_Noeud *m=pere(abr,n);
+    while((m!=NULL) && (n=m->droite)){
+        n = m;
+        m = pere(abr,m);
+    }
+    return m;
+}
+
+void decrementerElement(T_Arbre *abr , int cle){
+   T_Noeud *n=rechercherElement(abr,cle);
+   n->nb_occ--;
+   if(n->nb_occ == 0){
+      if(n->gauche==NULL && n->droite==NULL){   // pas de fils
+         if(pere(abr,n)->gauche == n){pere(abr,n)->gauche=NULL;}
+         else if(pere(abr,n)->droite == n){pere(abr,n)->droite=NULL;}
+         else if(pere(abr,n) == NULL){abr->Racine=NULL;}
+         free(n);
+      }
+      else if(n->gauche==NULL && n->droite!=NULL){   //un seul fil(droite)
+         if(pere(abr,n)->gauche == n){pere(abr,n)->gauche=n->droite;}
+         else if(pere(abr,n)->droite == n){pere(abr,n)->droite=n->droite;}
+         else if(pere(abr,n) == NULL){abr->Racine=n->droite;}
+         free(n);
+      }
+      else if(n->gauche!=NULL && n->droite==NULL){  //un seul fil(gauche)
+         if(pere(abr,n)->gauche == n){pere(abr,n)->gauche=n->gauche;}
+         else if(pere(abr,n)->droite == n){pere(abr,n)->droite=n->gauche;}
+         else if(pere(abr,n) == NULL){abr->Racine=n->gauche;}
+         free(n);
+      }
+      else if(n->gauche!=NULL && n->droite!=NULL){  //deux fils
+         T_Noeud *succ=successeur(abr,n);
+         succ->gauche=n->gauche;
+         succ->droite=n->droite;
+         if(pere(abr,n)->gauche == n){pere(abr,n)->gauche=succ;}
+         else if(pere(abr,n)->droite == n){pere(abr,n)->droite=succ;}
+         else if(pere(abr,n) == NULL){abr->Racine=succ;}
+         if(pere(abr,succ)->gauche==succ){pere(abr,succ)->gauche=NULL;}
+         else if(pere(abr,succ)->droite==succ){pere(abr,succ)->droite=NULL;}
+         free(n);
+      }
+   }
+}
