64103fbcfc7858cd51dfec43f115bbad17e49fef
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-05-16 02:48:49 +0200

    Update tp4.h

--- nf16/tp4/tp4.h
+++ nf16/tp4/tp4.h
@@ -7,23 +7,23 @@
 typedef struct Noeud{
     int cle;
     int nb_occ;
-    struct Noeud *gauche;
-    struct Noeud *droite;
+    struct Noeud* gauche;
+    struct Noeud* droite;
 }T_Noeud;
 
 typedef struct arbre{
-    T_Noeud *Racine;
+    T_Noeud* Racine;
 }T_Arbre;
 
 T_Arbre* initABR();
 
-T_Noeud *creerNoeud(int cle);
+T_Noeud* creerNoeud(int cle);
 
-void ajoutElement(T_Arbre *abr , int cle);
+void ajoutElement(T_Arbre* abr,int cle);
 
-void afficherArbre(T_Noeud *n);
+void afficherArbre(T_Noeud* n);
 
-T_Noeud *rechercherElement(T_Arbre *abr , int val);
+T_Noeud* rechercherElement(T_Arbre* abr,int val);
 
-void decrementerElement(T_Arbre *abr,int cle);
+void decrementerElement(T_Arbre* abr,int cle);
 #endif
